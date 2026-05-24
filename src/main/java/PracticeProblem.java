62f1fa4536e859e3344e0de9286774bad5ccf91f
weiyaliu <343445839@tdsb.ca>
2026-05-23 21:20:42 -0400

    doen i think
    Added methods to swap elements in an ArrayList, create an ArrayList from an array, and merge two ArrayLists.

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,54 @@
+/**
+	* Lesson: ArrayLists
+	* Author: Weiya
+	* Date Created: May 10, 2026
+	* Date Last Modified: May 23, 2026
+	*/
+
+import java.util.ArrayList;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
+	//1.
+	public static void swap (ArrayList<String> list, int index1, int index2) {
+	    
+	    String temp = list.get(index1);
+	    
+	    list.set(index1, list.get(index2));
+	    
+	    list.set (index2, temp);
+	}
+
+	//2.
+	public static ArrayList<Double> creatArrayList(double[ ] arr) {
+	    
+	    
+	    ArrayList<Double> list = new ArrayList<Double>();
+	    
+	    for (int i= 0; i< arr.length; i++) {
+	        list.add(arr[i]);
+	    }
+	    
+	    return list;
+	}
+
+	//3.
+	public static ArrayList<Integer> mergeLists(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+	    
+        ArrayList<Integer> merged = new ArrayList<Integer>();
+
+        for (int i= 0; i< list1.size; i++) {
+            merged.add(list1.get(i))
+        }
+        
+        for (int i= 0; i< list2.size; i++) {
+            merged.add(list2.get(i))
+        }
+        
+        return merged
+	}
 }
